1c6f538ce21adc2d566fc15aa83579d0c04bfe0d
root <root@anubiz.anubiz.lan>
2022-02-11 00:15:34 -0500

    AS: insert sql in database

--- control/validar.php
+++ control/validar.php
@@ -5,14 +5,14 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
- require_once '../logica/clases/Persona.php';
- require_once '../logica/clasesGenericas/ConectorBD.php';
-$usuario=$_REQUEST['usuario'];
-$clave=$_REQUEST['clave'];
-$usuario=Persona::validar($usuario, $clave);
-if ($usuario==null) header('location:../index.php?mensaje=usuario o contraseña no valido');
+require_once '../logica/clases/Persona.php';
+require_once '../logica/clasesGenericas/ConectorBD.php';
+$usuario = $_REQUEST['usuario'];
+$clave = $_REQUEST['clave'];
+$usuario = Persona::validar($usuario, $clave);
+if ($usuario == null) header('location:../index.php?mensaje=usuario o contraseña no valido');
 else {
     session_start();
-    $_SESSION['usuario']=serialize($usuario);
+    $_SESSION['usuario'] = serialize($usuario);
     header('location: ../principal.php?CONTENIDO=presentacion/inicio.php');
 }
