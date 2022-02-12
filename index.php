04ad321892a0789a781a29fc056c464c036c88fa
Edgar Muñoz <edgmuo1085@gmail.com>
2022-02-12 01:36:32 -0500

    AS-12: se agregan estilos en index principal

--- index.php
+++ index.php
@@ -1,53 +1,87 @@
 <!DOCTYPE html>
 <?php
-    session_start();
-    session_unset();
-    session_destroy();
+session_start();
+session_unset();
+session_destroy();
 
-    $mensaje='';
-    if(isset($_REQUEST['mensaje'])) $mensaje=$_REQUEST['mensaje'];
+$mensaje = '';
+if (isset($_REQUEST['mensaje'])) $mensaje = $_REQUEST['mensaje'];
 ?>
 <html>
-    <head>
-        <meta charset="windows-1252">
-        <title>SIDCA SISTEMA DE CONTROL ACADEMICO</title>
-        
-        <link rel="stylesheet" type="text/css" href="presentacion/css/master.css">
-    </head>
-    <body background="presentacion/imagenes/1.jpg">
-    <center>
-        <img class="foto1" src="presentacion/imagenes/pixlr-bg-result.png" align="left"/> 
-        <div id="banner">
-        <h3>COLEGIO LOS ANDES NUESTRA SEÑORA DE LAS MERCEDES</h3>
-        Institución educativa con 35 años de trayectoria, basada en la educación integral
-        y personalizada. Contamos con altos estándares en educación, donde hacemos énfasis a
-        nuestro lema "NADA ES TAM IMPORTANTE COMO UN NIÑO".</p>
-        </div>
-     
-        <p><font color='red'><?=$mensaje?></font></p>
-       
-        <form name="formulario" method="post" action="control/validar.php"> 
-        <div class="login-box">
-            <img class="avatar" src="presentacion/imagenes/candidato_1.png" alt="logo">
-            <h3>SISTEMA DE CONTROL ACADEMICO</h3>
-            <h3>LOGIN</h3>
-                <label for="username">Usuario</label>
-                <input type="text" name="usuario" placeholder="Enter Username">
-                <!-- PASSWORD -->
-                <label for="password">Clave</label>
-                <input type="password" name="clave" placeholder="Enter Password">
-                
-        <input type="submit" value="Ingresar"/>
-        </form>
-        
-        
-        <p>
-        </div>
-            <img src="presentacion/imagenes/logo.png" width="600" height="400" border="0">
-    </center>
-    
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>.::SIDCA - SISTEMA DE CONTROL ACADÉMICO::.</title>
+
+	<link rel="stylesheet" type="text/css" href="presentacion/css/main.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" />
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,700;1,100;1,400;1,700&display=swap" rel="stylesheet">
+</head>
+
+<body>
+	<header>
+		<nav class="as-nav-header">
+			<div>
+				<img class="as-logo" src="presentacion/imagenes/pixlr-bg-result.png" alt="logo" />
+			</div>
+			<div class="as-information">
+				<h3 class="as-title">COLEGIO LOS ANDES NUESTRA SEÑORA DE LAS MERCEDES</h3>
+				<p>Institución educativa con 35 años de trayectoria, basada en la educación integral
+					y personalizada. Contamos con altos estándares en educación, donde hacemos énfasis a
+					nuestro lema "NADA ES TAM IMPORTANTE COMO UN NIÑO".</p>
+			</div>
+		</nav>
+	</header>
+	<main class="as-content">
+		<!-- <div class="as-msg-error"><span>Acceso denegado</span></div> -->
+		<?php
+		if ($mensaje) {
+		?>
+			<div class="as-msg-error"><span><?= $mensaje ?></span></div>
+		<?php } ?>
+		<div>
+			<form class="as-form" name="formulario" method="post" action="control/validar.php" autocomplete="off">
+				<div class="as-form-avatar">
+					<img src="presentacion/img/avatar.png" alt="logo">
+				</div>
+				<div class="as-form-margin">
+					<h2>SISTEMA DE CONTROL ACADEMICO</h2>
+					<h4>Iniciar sesión</h4>
+					<div class="as-form-fields">
+						<div class="as-form-input">
+							<label for="usuario">Usuario</label>
+							<input type="text" name="usuario" id="usuario" placeholder="Ingresar usuario">
+						</div>
+
+						<div class="as-form-input">
+							<!-- PASSWORD -->
+							<label for="clave">Clave</label>
+							<input type="password" name="clave" id="clave" placeholder="Ingresar clave">
+						</div>
+					</div>
+					<div class="as-form-submit">
+						<button type="submit">
+							Ingresar
+						</button>
+					</div>
+				</div>
+			</form>
+		</div>
+	</main>
+	<footer class="as-footer">
+		<p>Derechos Reservados</p>
+		<p>
+			<script>
+				date = new Date().getFullYear();
+				document.write(date);
+			</script>
+		</p>
+		<p>&copy;</p>
+	</footer>
 </body>
+
 </html>
-
-
-    
